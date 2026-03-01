5ec7ceb39f4a1cf40d2fd0f40b20386ee53708c0
MatheusMarques0 <160657488+MatheusMarques0@users.noreply.github.com>
2026-03-01 02:06:46 -0300

    começando a usar o tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,38 @@
-import { useState } from "react"
+import { useState } from "react";
+import AddTask from "./components/AddTasks";
+import Tasks from "./components/Tasks";
 
 function App(){
-  const [message, setMessage] = useState("Olá mundo!")
+  const [tasks, setTasks] = useState([
+     {
+      id: 1,
+      title: "Estudar programação",
+      description: "Estudar programação para se tornar um desenvolvedor full stack.",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Estudar inglês",
+      description: "Estudar inglês para se tornar fluente.",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Estudar matemática",
+      description: "Estudar matemática para se tornar um desenvolvedor full stack.",
+      isCompleted: false
+    }
+  ])
 
   return (
-    <div>
-      <h1>{message}</h1>
-      <button onClick={() => {
-        setMessage("Olá, fui clicado!")
-      }}
-      >
-        Mudar mensagem
-      </button>
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+    <div className="w-[500px]">
+      <h1 className="text-3xl text-slate-100 font-bold text-center">
+        Gerendciador de Tarefas
+        </h1>
+        <AddTask />
+      <Tasks tasks={tasks}/>
+    </div>
     </div>
   );
 }
